3747c9aecc42dd2a8bc11ff3cf4e785e9cacba37
Sachinthaka Abeywardana <sachin.abeywardana@gmail.com>
2015-10-17 18:09:44 +1100

    documented the Rjmcmc code

--- matlab_code/demoRjmcmc.m
+++ matlab_code/demoRjmcmc.m
@@ -10,6 +10,7 @@ b = 1;
 iterations=20000;
 burnin=iterations*0.1;
 
+% true generative model
 rng(1);
 pi = [0.4, 0.6];
 lambda_ = [2, 6];
@@ -17,42 +18,41 @@ z = mnrnd(1, pi, N);
 y = gamrnd(1, 1./(z*lambda_'),N, 1);
 extremes = [min(1./y), max(1./y)];
 
+%MCMC scheme to find posteriors
 [lambda1, lambda2, states] = posteriorRjmcmc(y, K, extremes, iterations);
 
+% find how many are from state 1 and 2
 states=states(burnin:end);
 state1 = sum(states == 1);
 state2 = sum(states == 2);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% likelihood
+% exact marginal likelihood
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% p(y|k=2)
 iter = 1e6;
 lambda12 = jeffreysPrior(iter, extremes);
 lambda22 = jeffreysPrior(iter, extremes);
 pi = betarnd(alpha, alpha, iter, 1);
-log_py = zeros(iter, 1);
-normC = diff(log(extremes));
+log_py2 = zeros(iter, 1);
 
 for i= 1:iter
     a = log(pi(i))+log(lambda12(i))-lambda12(i)*y;
     b = log(1-pi(i))+log(lambda22(i))-lambda22(i)*y;
     log_pyi = [a b];
-    log_py(i) = sum(logsumexp(log_pyi, 2));
+    log_py2(i) = sum(logsumexp(log_pyi, 2));
 end
 
-% a = log(pi)+log(lambda12)-lambda12*y.T
-% b = log(1-pi)+log(lambda22)-lambda22*y.T
-
-log_py_k2 = logsumexp(log_py, 1)-log(iter);
+log_py_k2 = logsumexp(log_py2, 1)-log(iter);
+% p(y|k=1)
+normC = diff(log(extremes));
 log_py_k1 = -log(normC)-N*log(sum(y))+gammaln(N)+log(diff(gammainc(sum(y)*extremes,N)));
 
 p_k1 = 1./(1.+exp(log_py_k2-log_py_k1));
-disp(p_k1);
-disp(state1/(state1+state2));
-% plt.figure()
-% plt.plot(lambda1)
-% plt.show()
-% plt.figure()
-% plt.plot(lambda2[:, 0], lambda2[:, 1], '.')
-% plt.show()
-
+disp(strcat('exact posterior of p(k=1|y): ',num2str(p_k1)));
+disp(strcat('simulated posterior of p(k=1|y): ',num2str(1/(state1+state2))));
+% figure()
+% plot(lambda1)
+% figure()
+% plot(lambda2[:, 0], lambda2[:, 1], '.')
--- matlab_code/posteriorRjmcmc.m
+++ matlab_code/posteriorRjmcmc.m
@@ -4,27 +4,29 @@ alpha = 5;
 N = length(y);
 
 normC = diff(log(extremes));
-state = 1;
 
-lambda1 = extremes(1)+(extremes(2)-extremes(1))*rand;
+% allocate space for lambda1/2 chains and state transitions
 lambda1_chain = zeros(iterations*2, 1);
 lambda2_chain = zeros(iterations*2, 2);
-
 state_transition = zeros(iterations*2+1, 1);
-state_transition(1) = state;
 
-idx1 = 1;
-idx2 = 1;
+%randomly generate very first iteration as state 1
+state = 1; 
+idx1 = 1; idx2 = 1; %index keepers
+lambda1 = extremes(1)+(extremes(2)-extremes(1))*rand;
+state_transition(1) = state;
 lambda1_chain(idx1) = lambda1;
+
 idx1 = idx1 +1;
 l=2;
 for i =1:iterations
-    if state == 1
+    % jump proposals from current state to new state along with new lambdas
+    if state == 1  % q(2 to 1)
         lambda1 = lambda1_chain(idx1-1);
         mu = rand(2,1);
         lambda2 = [lambda1*mu(1)/(1-mu(1)),  lambda1*(1-mu(1))/mu(1)];
         pi_12 = [mu(2), 1-mu(2)];
-    else % state 2
+    else % state 2 (q 1 to 2)
         lambda2 = lambda2_chain(idx2-1,:);
         lambda1 = sqrt(prod(lambda2));
         mu(1) = lambda1/(lambda1+lambda2(2));
@@ -45,33 +47,26 @@ for i =1:iterations
         if state == 2
             state = 1;  % switch states
             lambda1_chain(idx1) = lambda1;
-            % new values of lambda (birth step?)
-            lambda1 = q_lambda(y, extremes);
-            lambda1_chain(idx1+1) = lambda1;
+            % Gibbs step
+            lambda1 = q_lambda(y, extremes); %gibbs step
+            lambda1_chain(idx1+1) = lambda1; 
             idx1 =idx1+2;
         else  % state 1
             state = 2;  % switch states
             lambda2_chain(idx2,:) = lambda2;
-            % new values of lambda (birth step?)
-            z = q_z2(y, pi_12, lambda2);
-            n_k = sum(z);
-            lambda2 = q_lambda2(y, z, n_k, extremes);
-            pi_12 = q_pi2(n_k, alpha);
+            [lambda2, pi_12]=gibbs_sampler2(y, pi_12, lambda2, alpha, extremes);
             lambda2_chain(idx2+1,:) = lambda2;
             idx2 = idx2+2;
         end
-    else  % keep old value
+    else  % if rejected proposal, keep old value
         if state == 2
             lambda2_chain(idx2,:) = lambda2;
-            z = q_z2(y, pi_12, lambda2);
-            n_k = sum(z);
-            lambda2 = q_lambda2(y, z, n_k, extremes);
-            pi_12 = q_pi2(n_k, alpha);
+            [lambda2, pi_12]=gibbs_sampler2(y, pi_12, lambda2, alpha, extremes);
             lambda2_chain(idx2+1,:) = lambda2;
             idx2 = idx2+2;
         else
             lambda1_chain(idx1) = lambda1;
-            lambda1 = q_lambda(y, extremes);
+            lambda1 = q_lambda(y, extremes); %gibbs step
             lambda1_chain(idx1+1) = lambda1;
             idx1=idx1+2;
         end
@@ -114,16 +109,8 @@ endPoints = gammainc(gam_b.*extremes,gam_a);
 lambda_const = diff(endPoints);
 lambda = gammaincinv(endPoints(1)+u*lambda_const,gam_a)/gam_b;
 
-function r = drchrnd(a,n)
-% take a sample from a dirichlet distribution
-p = length(a);
-r = gamrnd(repmat(a,n,1),1,n,p);
-r = r ./ repmat(sum(r,2),1,p);
-
-function p=normalise(logp)
-%     returns exp(logp)/sum(exp(logp)) without numerical 
-%     problems for a NxD matrix
-max_logp=max(logp,[],2);
-logp=bsxfun(@minus,logp,max_logp);
-p=exp(logp);
-p=bsxfun(@rdivide,p,sum(p,2));
+function [lambda2, pi_12]=gibbs_sampler2(y, pi_12, lambda2, alpha, extremes)
+z = q_z2(y, pi_12, lambda2);
+n_k = sum(z);
+lambda2 = q_lambda2(y, z, n_k, extremes);
+pi_12 = q_pi2(n_k, alpha);
