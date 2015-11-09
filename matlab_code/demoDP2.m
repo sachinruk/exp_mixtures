741ce38b7d14a24c5fb937a1ab5676cf0920ca19
Sachinthaka Abeywardana <sachin.abeywardana@gmail.com>
2015-11-09 14:58:36 +1100

    finished DP VB write up

--- matlab_code/demoDP2.m
+++ matlab_code/demoDP2.m
@@ -2,12 +2,12 @@ clear all
 clc
 close all
 
-alpha = 0.5;
-K = 2;
+alpha = 1;
+% K = 2;
 N = 4000;
 a = 1;
 b = 1;
-iterations=500;
+iterations=2000;
 burnin=iterations*0.1;
 
 % CRP generative model
@@ -30,7 +30,7 @@ extremes = [min(1./y), max(1./y)];
 [z_inf,lambda] = DPposterior(y, extremes, iterations, alpha);
 
 %VB to find posterior
-[phi_z, Elambda,class_vb]=DPVB(y,1000);
+[phi_z, Elambda,class_vb]=DPVB(y,500);
 unique(class_vb)
 
 
@@ -38,17 +38,30 @@ c=zeros(iterations,1);
 for i=1:iterations
     c(i)=length(unique(z_inf(:,i)));
 end
-disp(max(c))
+% disp(max(c))
 
 figure; plot(c); axis([1 iterations 0 max(c)])
 figure; hist(c);
 
 mcmc_c=confusionmat(z,z_inf(:,end));
 vb_c=confusionmat(z,class_vb);
-nmi(z,z_inf(:,end))
-nmi(z,class_vb)
-ari(z,z_inf(:,end))
-ari(z,class_vb)
+
+nmi_mcmc=zeros(size(z_inf,2),1);
+ari_mcmc=zeros(size(z_inf,2),1);
+for i=1:size(z_inf,2)
+    nmi_mcmc(i)=nmi(z,z_inf(:,i));
+end
+nmi_vb=nmi(z,class_vb);
+% ari(z,z_inf(:,end))
+for i=1:size(z_inf,2)
+    ari_mcmc(i)=ari(z,z_inf(:,i));
+end
+ari_vb=ari(z,class_vb);
+
+figure; plot(nmi_mcmc); hold on; plot([1 iterations],[nmi_vb nmi_vb],'r')
+figure; plot(ari_mcmc); hold on; plot([1 iterations],[ari_vb ari_vb],'r')
+
+disp(length(unique(z)));
 % z_inf2 = sparse(1:N,z_inf(:,4687),1); %convert z to a indicator matrix
 % lof_z_true=lof(z_true); lof_z_inf2=lof(z_inf2);
 % figure; subplot(121); imagesc(lof_z_true);
